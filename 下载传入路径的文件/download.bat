d4b4c8fbd69bc23ada17e6aa12173a25eec348e0
谢杰信 <1102171176@qq.com>
2026-05-11 11:06:11 +0800

    更新

--- 下载传入路径的文件/download.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-start "" /b powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0download.ps1"
--- 下载传入路径的文件/start.ps1
+++
@@ -1,2 +0,0 @@
-$psPath = Join-Path $PSScriptRoot "download.ps1"
-Start-Process powershell -ArgumentList "-NoProfile", "-ExecutionPolicy", "Bypass", "-File", "`"$psPath`"" -WindowStyle Hidden
